badd586eef2c8c505e94b6838fdccd68afd14133
Joshua Costanza <joshuacostanza@Joshuas-MBP.lan>
2025-04-03 06:36:31 -0400

    Set layout width to custom max-w-[1440px]

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <div className="max-w-6xl mx-auto px-6">
+      <div className="max-w-[1440px] mx-auto px-6">
         <Homepage />
         <Portfolio />
         <div id="contact">
